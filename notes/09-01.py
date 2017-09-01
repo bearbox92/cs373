43a8baa4436bf5b01f031166f8fd3e1befb37850
Glenn Downing <downing@cs.utexas.edu>
2017-09-01 18:42:56 -0500

    another commit

--- notes/09-01.py
+++ notes/09-01.py
@@ -25,4 +25,12 @@ assertions are good for preconditions and postconditions
 assertions are good for progammer errors
 assertions are NOT good for testing
 assertions are NOT good for user errors
+
+Questions:
+    When running a Docker image can you map a part of the local file system to a part of the file system in Docker?
+    What's the bug in the cycle_length function that we studied today?
+    When an assertion fails, what information does it provide?
+    What turns off all assertions?
+    Why are assertions bad for testing?
+    Why are assertions bad for user errors?
 """
