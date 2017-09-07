d0863be4445279d198a60a8d031ba0c9f261029d
Glenn Downing <downing@cs.utexas.edu>
2017-09-06 21:14:54 -0500

    another commit

---
+++ notes/09-06.py
@@ -0,0 +1,25 @@
+# -----------
+# Wed,  6 Sep
+# -----------
+
+"""
+Docker
+assertions
+    not good for unit testing
+    not good for user error
+
+Collatz
+    workflow
+    rubrics
+    code
+
+tests
+    black box
+        acceptance tests
+        test overall behavior
+        for Collatz it's two files: an input and an ouput
+    white box
+        unit tests
+
+read eval print loop (REPL)
+"""
