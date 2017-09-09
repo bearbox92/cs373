0b78728c5b18ebbdefad93122aa80fdd6296335d
Glenn Downing <downing@cs.utexas.edu>
2017-09-09 14:03:26 -0500

    another commit

---
+++ notes/09-08.py
@@ -0,0 +1,56 @@
+# -----------
+# Fri,  8 Sep
+# -----------
+
+"""
+(3n + 1) / 2
+3n/2 + 1/2
+n + n/2 + 1/2
+n + n/2 + 1
+
+b = 10
+e = 100
+m = 51  mcl(10, 100) = mcl(51, 100)
+
+b = 200
+e = 300
+m = 151 no claim
+
+Collatz
+    workflow
+    rubrics
+    code
+
+unit testing with Python's unittest
+coverage
+Collatz optimizations
+
+where exactly in Java is this available and not available
+    available     in non-static methods
+    not available in     static methods
+
+1 1,000,000 525
+
+what if: 234234 -> 525
+
+cl(5)  -> 6
+cl(10) -> 7
+
+mcl(1, 10) -> how much of the cache gets filled?
+
+fill the cache in response to a query, a LAZY cache
+
+fill the cache before any query, an EAGER cache
+
+META cache
+
+instead of caching cycle lengths, we cache max cycle lengths
+
+mcl(   1, 1000)
+mcl(1001, 2000)
+mcl(2001, 3000)
+mcl(3001, 4000)
+...
+
+what's mcl(500, 2500)
+"""
